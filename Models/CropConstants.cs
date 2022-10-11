354952b8aed2423d171348080dc3a694775e0e65
TheWeion <ianterryfallows@gmail.com>
2022-10-11 19:38:24 +0100

    API Restructure and JWT Integrated into the application.

---
+++ Models/CropConstants.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CropManagementAPI.Models
+{
+	public class CropConstants
+	{
+		public static List<CropModel> Crops = new List<CropModel>()
+		{
+			new CropModel()
+			{
+				CropId = 1,
+				UserId = 1,
+				CropName = "Tomato",
+				DatePlanted = new System.DateTime(2020, 1, 1),
+				DateWatered = new System.DateTime(2020, 1, 1),
+				DateHarvested = new System.DateTime(2020, 1, 1)
+			},
+			new CropModel()
+			{
+				CropId = 2,
+				UserId = 2,
+				CropName = "Potato",
+				DatePlanted = new System.DateTime(2020, 1, 1),
+				DateWatered = new System.DateTime(2020, 1, 1),
+				DateHarvested = new System.DateTime(2020, 1, 1)
+			}
+		};
+	}
+}
---
+++ Models/UserConstants.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace CropManagementAPI.Models
+{
+	public class UserConstants
+	{
+		public static List<UserModel> Users = new List<UserModel>()
+		{
+			new UserModel()
+			{
+				UserId = 1,
+				Username = "admin",
+				Password = "admin",
+				EmailAddress = "admin@cropmail.com",
+				Role = "Administrator",
+				DisplayName = "Admin",
+				Bio = "I am the admin",
+				FavouriteCrops = new string[] { "Tomato", "Potato", "Cabbage" }
+			},
+			new UserModel()
+			{
+				UserId = 2,
+				Username = "user",
+				Password = "user",
+				EmailAddress = "user@cropmail.com",
+				Role = "User",
+				DisplayName = "User",
+				Bio = "I am a user",
+				FavouriteCrops = new string[] { "Tomato", "Potato", "Cabbage" }
+			}
+		};
+	}
+}
